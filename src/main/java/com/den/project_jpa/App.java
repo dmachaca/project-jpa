87717d969e9f3c0862a215f291863d3acdbbe21c
“dmachaca” <dmachacass@gmail.com>
2023-01-19 23:01:13 -0500

    Version inicial del proyecto project-jpa

---
+++ src/main/java/com/den/project_jpa/App.java
@@ -0,0 +1,13 @@
+package com.den.project_jpa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
